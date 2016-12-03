932e78931998d558c40a5f00a2923f14127672e5
Sam Stevens <sam@xnet.tk>
2016-12-03 21:43:04 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+if (php_sapi_name() == 'cli-server') {
+    $info = parse_url($_SERVER['REQUEST_URI']);
+    if (file_exists('./'.$info['path']) && is_file('./'.$info['path'])) {
+        return false;
+    }
+}
+
+require 'vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+
+    $r->addRoute('GET', '/', function() {
+        header('Content-Type: text/plain');
+        echo 'Hello World';
+    });
+
+});
+
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+//strip query string
+if (($qPos = strpos($uri, '?')) !== false) {
+    $uri = substr($uri, 0, $qPos);
+}
+
+$route = $dispatcher->dispatch($httpMethod, $uri);
+switch($route[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+
+        header('Content-Type: text/plain', true, 404);
+        echo '404 - Not Found';
+
+        break;
+
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+
+        header('Content-Type: text/plain', true, 405);
+        echo "405 - Method Not Allowed\nAllowed: ".join(", ", $route[1]);
+
+        break;
+
+    case FastRoute\Dispatcher::FOUND:
+
+        $handler = $route[1];
+        $vars = $route[2];
+        call_user_func_array($handler, $vars);
+
+        break;
+
+}
